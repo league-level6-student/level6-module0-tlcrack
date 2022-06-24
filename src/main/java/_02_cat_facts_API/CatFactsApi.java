952bb9ebeaa665803241163a8a32d0be97b2a9ba
League Student <league@iMac-24.attlocal.net>
2022-06-23 18:28:50 -0700

    CatFactsApi
    Hoooooooo boy

--- src/main/java/_02_cat_facts_API/CatFactsApi.java
+++ src/main/java/_02_cat_facts_API/CatFactsApi.java
@@ -4,6 +4,8 @@ import _02_cat_facts_API.data_transfer_objects.CatWrapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import _01_intro_to_APIs.data_transfer_objects.Result;
 import reactor.core.publisher.Mono;
 
 /*
@@ -34,16 +36,19 @@ public class CatFactsApi {
         Use the WebClient to make the request, converting the response to String.class.
         This request doesn't require url parameters, so you can omit the .uri() method call entirely
         */
-
+    	Mono<String> stringMono = webClient
+    			.get()
+    			.retrieve()
+    			.bodyToMono(String.class);
 
         //Collect the response from the Mono object
-
+    	String response = stringMono.block();
 
         /*
         Print out the actual JSON response -
         this is what you will input into jsonschema2pojo.com
          */
-
+    	System.out.println(response);
 
         /*
         Use http://www.jsonschema2pojo.org/ to generate your POJO
@@ -56,24 +61,27 @@ public class CatFactsApi {
         */
     }
 
-    public String getCatFact() {
+    public CatWrapper getCatFact() {
 
         //Make the request, saving the response in an object of the type that you just created in your
         //data_transfer_objects package (CatWrapper)
-
+    	Mono<CatWrapper> catWrapperMono = webClient
+                .get()
+                .retrieve()
+                .bodyToMono(CatWrapper.class);
         //Use block() to collect the response into a java object using the class you just created
-
+    			CatWrapper cw = catWrapperMono.block();
         //return the Object
-        return null;
+        return cw;
 
 
     }
 
     public String findCatFact(){
         //use the getCatFact method to retrieve a cat fact
-
+    	
         //return the first (and only) String in the Arraylist of data in the response
-        return null;
+        return getCatFact().getData().get(0);
     }
 
     public void setWebClient(WebClient webClient) {
--- src/main/java/_02_cat_facts_API/data_transfer_objects/CatWrapper.java
+++ src/main/java/_02_cat_facts_API/data_transfer_objects/CatWrapper.java
@@ -1,32 +1,46 @@
 package _02_cat_facts_API.data_transfer_objects;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+"data"
+})
+@Generated("jsonschema2pojo")
 public class CatWrapper {
 
-    @SerializedName("data")
-    @Expose
-    private List<String> data = null;
+@JsonProperty("data")
+private List<String> data = null;
+@JsonIgnore
+private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-    public List<String> getData() {
-        return data;
-    }
+@JsonProperty("data")
+public List<String> getData() {
+return data;
+}
 
-    public void setData(List<String> data) {
-        this.data = data;
-    }
+@JsonProperty("data")
+public void setData(List<String> data) {
+this.data = data;
+}
 
-    /*
-    This class could simply be defined as below, but the above code is what is supplied by jsonschema2pojo.com:
+@JsonAnyGetter
+public Map<String, Object> getAdditionalProperties() {
+return this.additionalProperties;
+}
 
-    private List<String> data = null;
-
-    public List<String> getData() {
-        return data;
-    }
-    */
+@JsonAnySetter
+public void setAdditionalProperty(String name, Object value) {
+this.additionalProperties.put(name, value);
+}
 
 }
